e52014c347728c4af926c2faf88372a5a3348054
rajeshbattula631 <rajesh.battula631@gmail.com>
2018-09-08 12:12:33 +0530

    Initial commits
    Signed-off-by: rajeshbattula631 <rajesh.battula631@gmail.com>

---
+++ hsc/src/main/java/com/example/demo/HscApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HscApplication.class, args);
+	}
+}
