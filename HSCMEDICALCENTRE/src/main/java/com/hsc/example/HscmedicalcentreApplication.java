3faf471c0e904615b7cdf5de75757e57b87336a4
rajeshbattula631 <rajesh.battula631@gmail.com>
2018-09-08 13:09:09 +0530

    added project directory

---
+++ HSCMEDICALCENTRE/src/main/java/com/hsc/example/HscmedicalcentreApplication.java
@@ -0,0 +1,12 @@
+package com.hsc.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HscmedicalcentreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HscmedicalcentreApplication.class, args);
+	}
+}
---
+++ HSCMEDICALCENTRE/src/test/java/com/hsc/example/HscmedicalcentreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hsc.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HscmedicalcentreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
